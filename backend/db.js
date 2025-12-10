600941f4783965f1b9b9d816c3110adedb1f4957
averybrockington <168646662+averybrockington@users.noreply.github.com>
2025-12-09 19:37:36 -0500

    e

--- backend/db.js
+++ backend/db.js
@@ -5,5 +5,6 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
+pool.on('connect', () => console.log('Connected to PostgreSQL'));
 
 export default pool;
